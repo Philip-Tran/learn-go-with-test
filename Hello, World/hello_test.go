7076f3ce9511a0b84c097b593e35ff6c16ceb1d1
Quyet's Code <quyetcoccoc@gmail.com>
2025-04-27 10:15:22 +0700

    hello, world

---
+++ Hello, World/hello_test.go
@@ -0,0 +1,31 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("Print when there is a name", func(t *testing.T) {
+		got := Hello("Quyet", "")
+		want := "Hello, Quyet"
+	
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("Print when there is NOT a name", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+	
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("Spanish", func(t *testing.T) {
+		got := Hello("Quyet", "Spanish")
+		want := "Hola, Quyet"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
